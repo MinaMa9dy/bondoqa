d911e992d730ff98f218639cb6a031a3901f0b2d
mina <mandymina789@gmil.com>
2026-04-07 18:56:47 +0200

    measureid

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CartProvider } from './context/CartContext';
 import ReactGA from "react-ga4";
 
 // Initialize GA4 with the Measurement ID
-ReactGA.initialize("G-DBXHSFXGYK");
+ReactGA.initialize("G-ZZ95XGZPMN");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
